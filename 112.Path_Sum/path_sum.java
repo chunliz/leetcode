f1f0854f0f8fe18026c94ec2045e449c61d01b5b
Chunli Zhang <chunlizhang@lawn-128-61-29-173.lawn.gatech.edu>
2017-09-26 11:13:33 -0400

    add solution for P112

---
+++ 112.Path_Sum/path_sum.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        if(root.left==null && root.right==null) return sum==root.val?true:false;
+        else return hasPathSum(root.left,sum-root.val) || hasPathSum(root.right,sum-root.val);
+    }
+}
