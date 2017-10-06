060cb06127b46153ae7096c73f6145cd3fa67dc5
Chunli Zhang <chunlizhang@lawn-128-61-18-5.lawn.gatech.edu>
2017-10-06 11:01:03 -0400

    add solution for P284

---
+++ 284.PeekingIterator/peeking_iterator.java
@@ -0,0 +1,5 @@
+/*
+ The idea is to cache the next element. And employ the internal Iterator, and its methods next(), hasNext().
+ References:
+ https://discuss.leetcode.com/topic/24883/concise-java-solution/2
+*/
