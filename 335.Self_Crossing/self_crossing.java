714ab8f6138bc21aef32dc5bf090ce464ce1b831
Chunli Zhang <chunlizhang@lawn-128-61-25-252.lawn.gatech.edu>
2017-11-09 12:27:38 -0500

    add solution for P335

---
+++ 335.Self_Crossing/self_crossing.java
@@ -0,0 +1,21 @@
+/*
+Keywordds: Math, patterns
+ references:
+ https://discuss.leetcode.com/topic/38014/java-oms-with-explanation/2
+ */
+
+class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        if(x.length<=3) return false;
+        for(int i=3;i<x.length;i++){
+            if(x[i]>=x[i-2] && x[i-1]<=x[i-3]) return true;
+            if(i>=4){
+                if(x[i-1]==x[i-3] && x[i]+x[i-4]>=x[i-2]) return true;
+            }
+            if(i>=5){
+                if(x[i-2]>x[i-4] && x[i]+x[i-4]>=x[i-2] && x[i-1]<x[i-3] && x[i-1]+x[i-5]>=x[i-3]) return true;
+            }
+        }
+        return false;
+    }
+}
