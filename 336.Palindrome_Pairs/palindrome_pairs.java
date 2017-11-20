8e9c1636300451968001169be7d7c1edd20ad4e6
Chunli Zhang <chunlizhang@lawn-128-61-30-90.lawn.gatech.edu>
2017-11-20 11:29:20 -0500

    add solution for P336

---
+++ 336.Palindrome_Pairs/palindrome_pairs.java
@@ -0,0 +1,53 @@
+/*
+ Keywords: Trie
+ References: https://discuss.leetcode.com/topic/39585/o-n-k-2-java-solution-with-trie-structure-n-total-number-of-words-k-average-length-of-each-word/2
+ */
+
+class Solution {
+    private static class TrieNode{
+        TrieNode[] next;
+        int index;
+        List<Integer> list;
+        TrieNode(){
+            next=new TrieNode[26];
+            index=-1;
+            list=new ArrayList<>();
+        }
+    }
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> res=new ArrayList<>();
+        TrieNode root=new TrieNode();
+        for(int i=0;i<words.length;i++) addWord(root,words[i],i);
+        for(int i=0;i<words.length;i++) searchWord(words,i,root,res);
+        return res;
+    }
+    private void addWord(TrieNode root, String str, int index){
+        for(int i=str.length()-1;i>=0;i--){
+            int j=str.charAt(i)-'a';
+            if(root.next[j]==null) root.next[j]=new TrieNode();
+            if(isPalindrome(str,0,i)) root.list.add(index);
+            root=root.next[j];
+        }
+        root.list.add(index);
+        root.index=index;
+    }
+    private void searchWord(String[] words, int i, TrieNode root, List<List<Integer>> res){
+        for(int j=0;j<words[i].length();j++){
+            if(root.index>=0 && root.index!=i && isPalindrome(words[i],j,words[i].length()-1)) res.add(Arrays.asList(i,root.index));
+            
+            root=root.next[words[i].charAt(j)-'a'];
+            if(root==null) return;
+            
+        }
+        for(int j:root.list){
+            if(i==j) continue;
+            res.add(Arrays.asList(i,j));
+        }
+    }
+    private boolean isPalindrome(String word, int i, int j){
+        while(i<j){
+            if(word.charAt(i++)!=word.charAt(j--)) return false;
+        }
+        return true;
+    }
+}
