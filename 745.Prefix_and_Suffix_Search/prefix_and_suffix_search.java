3ee7f9bd119bfe3c231f91e34841ac63acff1287
Chunli Zhang <chunlizhang@lawn-128-61-16-91.lawn.gatech.edu>
2017-12-11 12:19:08 -0500

    add solution for P745

---
+++ 745.Prefix_and_Suffix_Search/prefix_and_suffix_search.java
@@ -0,0 +1,10 @@
+/*
+ Two methods:
+ 1. Map all the possible prefix+#+suffix to the corresponding weight;
+ 2. Build a Trie. Reduce the time of prefix search. With the obtained words, check their suffix one by one.
+ 
+ References:
+ https://discuss.leetcode.com/topic/113471/accepted-java-code-using-trie
+ */
+
+
