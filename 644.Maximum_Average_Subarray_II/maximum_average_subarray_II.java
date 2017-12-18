4b9fc21885eb0a251de2bedfa7d27f63abaafd6d
Chunli Zhang <chunlizhang@chunlis-mbp.attlocal.net>
2017-12-18 13:18:01 -0500

    add solution for P644

--- 644.Maximum_Average_Subarray_II/maximum_average_subarray_II.java
+++ 644.Maximum_Average_Subarray_II/maximum_average_subarray_II.java
@@ -34,3 +34,38 @@ class Solution {
         return true;
     }
 }
+
+/*
+Method 2:
+References: https://discuss.leetcode.com/topic/96131/python-advanced-o-n-solution-convex-hull-window/6
+ */
+
+/*
+ When we take index j as the end of the subarray, (j-k+1, j-k+2, ..., j) must be included. Now we consider whether we should include A=(0,1,2,...,j-k). Selected from A, we maintain a list {i_1,i_2,i_3...} which fulfils the following condition:
+ 1. aver(i_1,i_2-1) > aver(i_1,j);
+ 2. aver(i_n,j-k) > aver(i_(n-1),j-k) for any n.
+ 
+ In each step, we update the list to make it fulfil the above condition, and update the result if needed.
+*/
+class Solution {
+    int[] presum;
+    public double findMaxAverage(int[] nums, int k) {
+        double result=-1e5;
+        presum=new int[nums.length];
+        presum[0]=nums[0];
+        for(int i=1;i<nums.length;i++) presum[i]=presum[i-1]+nums[i];
+        LinkedList<Integer> ll=new LinkedList<>(); //nums[ll.get(ll.size()-1)]
+        for(int i=k-1;i<nums.length;i++){
+            while(ll.size()>=2 && aver(ll.get(ll.size()-1),i-k)<=aver(ll.get(ll.size()-2),i-k)) ll.pollLast();
+            ll.add(i-k+1);
+            while(ll.size()>=2 && aver(ll.get(0),ll.get(1)-1)<=aver(ll.get(0),i)) ll.pollFirst();
+            result=Math.max(result,aver(ll.get(0),i));
+        }
+        return result;
+    }
+    public double aver(int left, int right){
+        double result=0.0;
+        if(left==0) return (double)presum[right]/(right-left+1.0);
+        else return (double)(presum[right]-presum[left-1])/(right-left+1.0);
+    }
+}
